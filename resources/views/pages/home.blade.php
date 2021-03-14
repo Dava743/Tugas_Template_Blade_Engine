a3170173216c1be17eb04a46f83ea3eab7f4e82d
Dava743 <davaprmudia743@gmail.com>
2021-03-14 20:29:14 +0700

    Tugas template blade engine

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+@section('title', 'Halaman Home')
+@section('content')
+    <br>
+    <div class="jumbotron bg-light" style="margin-left: 20%; margin-top: 5%;">
+        <div style="font-family: arial;" class="text-center">
+            <h1 class="display-5 text-black" style=" font-family: arial; font-weight: bold;"><u>Selamat Datang Di OtakuDev</u></h1>
+            <h5 class="display-5" style="font-family: arial; font-weight: bold; margin: 0px; color: black;">Anime pertama yang mencapai kepopuleran yang luas adalah Astro Boy karya Ozamu Tezuka pada tahun 1963. Dewasa ini anime sudah sangat berkembang jika dibandingkan dengan anime zaman dulu. Dengan grafik yang sudah berkembang sampai alur cerita yang lebih menarik dan seru.
+
+</h5>
+        </div>
+    </div>
+<br><br>
+    <div class="card bg-light text-black" style="width: 700px; height: 200px; margin-left: 27%;">
+        <div class="card-img-overlay">
+            <h2 class="card-title"><u>Jujutsu Kaisen</u></h2>
+            <p class="card-text">adalah sebuah seri manga shōnen asal Jepang yang ditulis dan diilustrasikan oleh Gege Akutami. Manga ini dimuat berseri dalam majalah Weekly Shōnen Jump terbitan Shueisha sejak bulan Maret 2018, dan telah diterbitkan menjadi lima belas volume tankōbon per bulan Maret 2021.</p>
+        </div>
+    </div>
+
+    <div class="card bg-light text-black" style="width: 700px; height: 200px; margin-left: 27%; margin-top: 20px;">
+        <div class="card-img-overlay">
+            <h2 class="card-title"><u>One Piece</u></h2>
+            <p class="card-text">adalah sebuah seri manga Jepang yang ditulis dan diilustrasikan oleh Eiichiro Oda. Manga ini telah dimuat di majalah Weekly Shōnen Jump milik Shueisha sejak tanggal 22 Juli 1997, dan telah dibundel menjadi 91 volume tankōbon. Ceritanya mengisahkan petualangan Monkey D. Luffy, seorang anak laki-laki yang memiliki kemampuan tubuh elastis seperti karet setelah memakan Buah Iblis secara tidak disengaja.</p>
+        </div>
+    </div>
+
+    <div class="card bg-light text-black" style="width: 700px; height: 200px; margin-left: 27%; margin-top: 20px;">
+        <div class="card-img-overlay">
+            <h2 class="card-title"><u>Black Clover</u></h2>
+            <p class="card-text">adalah sebuah seri manga shōnen bergenre fantasi asal Jepang yang ditulis dan diilustrasikan oleh Yūki Tabata. Ceritanya mengisahkan tentang seorang anak laki-laki bernama Asta yang lahir tanpa kekuatan sihir, suatu fenomena yang tidak normal di dunia tempatnya tinggal.<p>
+        </div>
+    </div>
+@endsection
+
+@section('js')
+    <script>
+        console.log('Helo')
+    </script>
+@endsection
